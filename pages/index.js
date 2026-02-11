0db4f743755121c1478e934ce2600895425bdaae
Abhinandan Sethi <abhinandansethi07@gmail.com>
2026-02-12 02:25:29 +0530

    Trial

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     try {
       setLoading(true);
 
-      const res = await fetch("https://resumatch123.vercel.app/api/match", {
+      const res = await fetch("http://localhost:8000/api/match", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
